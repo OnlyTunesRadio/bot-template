b0f7ab7c36c66d33dd15a2722309f29b312ccdca
Brayd <info@onlytunes.uk>
2021-11-15 14:26:57 +0000

    Removed the old anti-crash mech

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -12,20 +12,6 @@ module.exports = {
      */
     run: async (client, message, args) => {
 
-        process.on('uncaughtException', (error, origin) => {
-            console.log('----- Uncaught exception -----')
-            console.log(error)
-            console.log('----- Exception origin -----')
-            console.log(origin)
-        })
-        
-        process.on('unhandledRejection', (reason, promise) => {
-            console.log('----- Unhandled Rejection at -----')
-            console.log(promise)
-            console.log('----- Reason -----')
-            console.log(reason)
-        })
-
         const WebSocketPing = client.ws.ping
         const MessagePing = Date.now() - message.createdTimestamp;
 
