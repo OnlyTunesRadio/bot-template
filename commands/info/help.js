ae1d9daf2bdc0c308ecee2f0c83c77cae24c6431
Brayd <info@onlytunes.uk>
2021-11-15 14:23:46 +0000

    Removed old anti-crash mech

--- commands/info/help.js
+++ commands/info/help.js
@@ -19,20 +19,6 @@ module.exports = {
      * @param {String[]} args
      */
     run: async (client, message) => {
-        process.on('uncaughtException', (error, origin) => {
-            console.log('----- Uncaught exception -----')
-            console.log(error)
-            console.log('----- Exception origin -----')
-            console.log(origin)
-        })
-        
-        process.on('unhandledRejection', (reason, promise) => {
-            console.log('----- Unhandled Rejection at -----')
-            console.log(promise)
-            console.log('----- Reason -----')
-            console.log(reason)
-        })
-
 
         // You should only have to ever touch this! 
         // This is only for commands with prefix! Slash commands dont need a help command!
