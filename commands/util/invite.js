a7ac7c0ed16d760c0c341d4b260ca9ed2ca85ad5
Brayd <info@onlytunes.uk>
2021-11-15 14:14:45 +0000

    Removed old anti-crash

--- commands/util/invite.js
+++ commands/util/invite.js
@@ -9,19 +9,6 @@ module.exports = {
      * @param {String[]} args
      */
     run: async (client, message) => {
-        process.on('uncaughtException', (error, origin) => {
-            console.log('----- Uncaught exception -----')
-            console.log(error)
-            console.log('----- Exception origin -----')
-            console.log(origin)
-        })
-        
-        process.on('unhandledRejection', (reason, promise) => {
-            console.log('----- Unhandled Rejection at -----')
-            console.log(promise)
-            console.log('----- Reason -----')
-            console.log(reason)
-        })
 
         const inviteURL = `https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=${process.env.PERMISSIONS}&scope=bot%20applications.commands`
 
